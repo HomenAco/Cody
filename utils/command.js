6f137f05963d078e7087cacb98a26f706c3184f5
HomenAco <42707791+HomenAco@users.noreply.github.com>
2019-06-27 16:09:11 -0300

    corrigindo alguns bug da opção instancia
    Adicionado um objeto que deve recuperar o diretório do seu comando.

--- utils/command.js
+++ utils/command.js
@@ -1,8 +1,9 @@
 module.exports = class command {
-    constructor(name, client) {
+    constructor(name, pathCommands,client) {
         this.name = name;
         this.client = client;
         this.aliases = [];
+        this.pathCommands = pathCommands
     }
     process({message, args, argsAlt, prefix, usuario, servidor}, t, setFixedT) {
         return this.run({message, args, argsAlt, prefix, usuario, servidor}, t, setFixedT);
